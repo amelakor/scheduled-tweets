85478c467a0f1b5df36a8d293fda221e7d5be451
KorjenicA <amela.korjenic@rokolabs.com>
2024-07-11 11:34:09 +0200

    Update password

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "sign_in" => "sessions#new"
   post "sign_in" => "sessions#create"
 
+  get "password" => "passwords#edit", as: 'edit_password'
+  patch "password" => "passwords#update"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
